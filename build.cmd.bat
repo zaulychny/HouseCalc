5598c8dc2f1065190af1fa121fcc39a0656346cb
zaulychny <zaulychny@yahoo.com>
2016-01-25 01:09:15 +0200

    Added fixed aplbuilder

--- build.cmd.bat
+++ build.cmd.bat
@@ -1,6 +1,10 @@
 ::=========================================================================================
 :: Build application APL WS using AplBuilder. It can even build itself from the sources 
 ::=========================================================================================
- C:\Data\Builds\aplbuilder.exe appname="house_calc"  bldroot="c:\Data\Builds" srcroot="C:\Data\SoftForge\HouseCalc" 
+:: Project C:\Data\SoftForge\AplBuilder\aplbuilder.exe
+aplbuilder.exe appname="house-calc"  bldroot="C:\Data\SoftForge\HouseCalc" srcroot="C:\Data\SoftForge\HouseCalc" 
+
+:: Released 
+::C:\Data\Builds\aplbuilder.exe appname="house-calc"  bldroot="c:\Data\Builds" srcroot="C:\Data\SoftForge\HouseCalc" 
 
 ::"C:\Program Files (x86)\Dyalog\Dyalog APL 14.1 Unicode\dyalog.exe" "C:\Data\SoftForge\AplBuilder\aplbuilder.dws" appname="newapldskapp" bldroot="c:\Data\Builds" srcroot="C:\Data\SoftForge\NewAplAppDsk" 
